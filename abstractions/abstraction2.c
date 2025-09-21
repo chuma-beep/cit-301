e67e9d59cd8c940d05379ed4a9abb0d366c1b5b1
chuma-beep <wisboynelson123@gmail.com>
2025-09-21 17:31:03 +0100

    modified abstractions.c

--- abstractions/abstraction2.c
+++ abstractions/abstraction2.c
@@ -1,32 +1,31 @@
-//second part of the abstracion example
- 
+// second part of the abstracion example
 
 #include <stdio.h>
 
-//function declaration (interface)
+// function declaration (interface)
 unsigned long long factorial(int n);
 
-//function definition (implementation)
+// function definition (implementation)
 unsigned long long factorial(int n) {
-    if (n < 0 || n > 20 ){ 
-        return 0;
-    }
-    if (n == 0 || n == 1){ 
-        return 1;
-    }
-    return n * factorial (n - 1);
+  if (n < 0 || n > 20) {
+    return 0;
+  }
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-int main(){
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    unsigned long long result = factorial(num);
+int main() {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  unsigned long long result = factorial(num);
 
-    if (result ==  0){
-        printf("Error the number is too large or negative.\n");
-    }else {
-        printf("Factorial of %d is %llu\n", num, result);
-    }
-    return 0;
+  if (result == 0) {
+    printf("Error the number is too large or negative.\n");
+  } else {
+    printf("Factorial of %d is %llu\n", num, result);
+  }
+  return 0;
 }
